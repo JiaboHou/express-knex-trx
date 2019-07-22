382f03386b1abae1bfbbf2ed40b3b0f731fe6396
Jiabo Hou <jiabo.hou@outlook.com>
2019-07-22 18:48:03 -0400

    completed tests, and tweaked implementation based on tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,305 @@
-const yo = require('../src').default;
+const httpMocks = require('node-mocks-http');
+const knex = require('knex');
+const mockKnex = require('mock-knex');
 
-describe('test test', () => {
-  it('returns "hi"', () => {
-    expect(yo(true)).toEqual('hi');
+const ekt = require('../src');
+
+describe('src/index', () => {
+  describe('knexTrxBegin', () => {
+    describe('when given invalid parameters', () => {
+      it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['a number', 4],
+        ['a string', 'hello'],
+        ['a boolean', true],
+        ['an array', []],
+        ['an object', {}],
+        ['a function without a "transaction" member', () => {}],
+      ])('throws a TypeError when parameter is %s', (label, testParam) => {
+        expect(() => ekt.knexTrxBegin(testParam)).toThrow(TypeError);
+      });
+    });
+
+    describe('when given valid paramters', () => {
+      const req = {};
+      const res = { locals: {} };
+      const next = jest.fn();
+      let beginTrxMiddleware;
+
+      beforeEach(() => {
+        const knexClient = knex({ client: 'pg' });
+        mockKnex.mock(knexClient);
+        beginTrxMiddleware = ekt.knexTrxBegin(knexClient);
+
+        next.mockReset();
+      });
+
+      it('returns a function', () => {
+        expect(beginTrxMiddleware).toBeInstanceOf(Function);
+      });
+
+      it('adds the transaction to res.locals', async () => {
+        next.mockImplementation(() => {
+          expect(res.locals).toHaveProperty('trx');
+        });
+        await beginTrxMiddleware(req, res, next);
+      });
+
+      it('proceeds to the next middleware', async () => {
+        await beginTrxMiddleware(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
   });
 
-  it('return "bye"', () => {
-    expect(yo(false)).toEqual('bye');
+  describe('knexTrxCommit', () => {
+    describe('when trx is not an instance of Knex.Transaction', () => {
+      const req = {};
+      const next = jest.fn();
+
+      beforeEach(() => {
+        next.mockReset();
+      });
+
+      it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['a number', 4],
+        ['a string', 'hello'],
+        ['a boolean', true],
+        ['an array', []],
+        ['an object', {}],
+        ['a function without a "commit" member', () => {}],
+      ])('throws an Error when res.locals.trx is %s', async (label, testParam) => {
+        await ekt.knexTrxCommit(req, { locals: { trx: testParam } }, next);
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+      });
+    });
+
+    describe('successful commit', () => {
+      const req = {};
+      const res = {
+        locals: {
+          trx: {
+            commit: jest.fn(),
+            rollback: jest.fn(),
+          },
+        },
+      };
+      const next = jest.fn();
+
+      beforeEach(() => {
+        ekt.knexTrxCommit(req, res, next);
+      });
+
+      it('attempts to commits the transaction', () => {
+        expect(res.locals.trx.commit).toHaveBeenCalled();
+      });
+
+      it('proceeds to the next middleware', () => {
+        expect(next).toHaveBeenCalled();
+      });
+
+      it('does not pass an error to the next middleware', () => {
+        expect(next.mock.calls[0][0]).not.toBeDefined();
+      });
+    });
+
+    describe('failed commit', () => {
+      const testErrorMessage = 'test error';
+      const req = {};
+      const res = {
+        locals: {
+          trx: {
+            commit: jest.fn(() => {
+              throw new Error(testErrorMessage);
+            }),
+            rollback: jest.fn(),
+          },
+        },
+      };
+      const next = jest.fn();
+
+      beforeEach(() => {
+        ekt.knexTrxCommit(req, res, next);
+      });
+      it('attempts to commits the transaction', () => {
+        expect(res.locals.trx.commit).toHaveBeenCalled();
+      });
+
+      it('proceeds to the next middleware', () => {
+        expect(next).toHaveBeenCalled();
+      });
+
+      it('passes an error to the next middleware', () => {
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+      });
+
+      it('passes the expected error message to the next middleware', () => {
+        expect(next.mock.calls[0][0].message).toEqual(testErrorMessage);
+      });
+    });
+  });
+
+  describe('knexTrxErrorHandler', () => {
+    describe('when trx is not an instance of Knex.Transaction', () => {
+      const req = {};
+      const next = jest.fn();
+
+      beforeEach(() => {
+        next.mockReset();
+      });
+
+      it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['a number', 4],
+        ['a string', 'hello'],
+        ['a boolean', true],
+        ['an array', []],
+        ['an object', {}],
+        ['a function without a "rollback" member', () => {}],
+      ])('throws an Error when res.locals.trx is %s', async (label, testParam) => {
+        await ekt.knexTrxErrorHandler(new Error(), req, { locals: { trx: testParam } }, next);
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+        expect(next.mock.calls[0][0]).toMatchSnapshot();
+      });
+    });
+
+    describe('successful rollback', () => {
+      const testErrorMessage = 'test error';
+      const req = {};
+      const res = {
+        locals: {
+          trx: {
+            commit: jest.fn(),
+            rollback: jest.fn(),
+          },
+        },
+      };
+      const next = jest.fn();
+
+      beforeEach(() => {
+        ekt.knexTrxErrorHandler(new Error(testErrorMessage), req, res, next);
+      });
+
+      it('attempts to rollback the transaction', () => {
+        expect(res.locals.trx.rollback).toHaveBeenCalled();
+      });
+
+      it('proceeds to the next middleware', () => {
+        expect(next).toHaveBeenCalled();
+      });
+
+      it('passes an error to the next middleware', () => {
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+      });
+
+      it('passes the expected error message to the next middleware', () => {
+        expect(next.mock.calls[0][0].message).toEqual(testErrorMessage);
+      });
+    });
+
+    describe('failed rollback', () => {
+      const testErrorMessage = 'test error';
+      const req = {};
+      const res = {
+        locals: {
+          trx: {
+            commit: jest.fn(),
+            rollback: jest.fn(() => {
+              throw new Error();
+            }),
+          },
+        },
+      };
+      const next = jest.fn();
+
+      beforeEach(() => {
+        ekt.knexTrxErrorHandler(new Error(testErrorMessage), req, res, next);
+      });
+
+      it('attempts to rollback the transaction', () => {
+        expect(res.locals.trx.rollback).toHaveBeenCalled();
+      });
+
+      it('proceeds to the next middleware', () => {
+        expect(next).toHaveBeenCalled();
+      });
+
+      it('passes an error to the next middleware', () => {
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+      });
+
+      it('passes the expected error message to the next middleware', () => {
+        expect(next.mock.calls[0][0].message).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('withKnexTrx', () => {
+    let knexClient;
+
+    beforeAll(() => {
+      knexClient = knex({ client: 'pg' });
+      mockKnex.mock(knexClient);
+    });
+
+    describe('when given invalid parameters', () => {
+      it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['a number', 4],
+        ['a string', 'hello'],
+        ['a boolean', true],
+        ['an array', []],
+        ['an object', {}],
+        ['a function without a "transaction" member', () => {}],
+      ])('throws a TypeError when "knex" parameter is %s', (label, testParam) => {
+        expect(() => ekt.withKnexTrx(testParam, () => {})).toThrow(TypeError);
+      });
+
+      it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['a number', 4],
+        ['a string', 'hello'],
+        ['a boolean', true],
+        ['an empty array', []],
+        ['an array of functions and primitives', [() => {}, 'a', 3, true]],
+        ['an object', {}],
+      ])('throws a TypeError when "middlewares" parameter is %s', (label, testParam) => {
+        expect(() => ekt.withKnexTrx(knexClient, testParam)).toThrow(TypeError);
+      });
+    });
+
+    it('adds 3 middlewares in addition to 1 user-specified middlewares', () => {
+      const middlewares = () => { return; };
+      const middlewaresWithKnexTrx = ekt.withKnexTrx(knexClient, middlewares);
+      expect(middlewaresWithKnexTrx.length).toEqual(4);
+    });
+
+    it('adds 3 middlewares in addition to an array of 1 user-specified middlewares', () => {
+      const middlewares = [() => { return; }];
+      const middlewaresWithKnexTrx = ekt.withKnexTrx(knexClient, middlewares);
+      expect(middlewaresWithKnexTrx.length).toEqual(4);
+    });
+
+    it('adds 3 middlewares in addition to an array of 3 user-specified middlewares', () => {
+      const middlewares = [() => { return; }, () => { return; }, () => { return; }];
+      const middlewaresWithKnexTrx = ekt.withKnexTrx(knexClient, middlewares);
+      expect(middlewaresWithKnexTrx.length).toEqual(6);
+    });
+
+    it('maintains ordering of the middlewares', () => {
+      const middlewares = [() => { return; }, () => { return; }, () => { return; }];
+      const middlewaresWithKnexTrx = ekt.withKnexTrx(knexClient, middlewares);
+      expect(middlewaresWithKnexTrx[1]).toBe(middlewares[0]);
+      expect(middlewaresWithKnexTrx[2]).toBe(middlewares[1]);
+      expect(middlewaresWithKnexTrx[3]).toBe(middlewares[2]);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,70 @@
-import { ErrorRequestHandler, RequestHandler } from 'express';
+import { ErrorRequestHandler, RequestHandler, Response } from 'express';
 import { RequestHandlerParams } from 'express-serve-static-core';
 import Knex from 'knex';
 
-export const knexTrxBegin = (knex: Knex): RequestHandler => {
+const checkKnex = (knex: Knex): void => {
   if (typeof knex !== 'function' || typeof knex.transaction !== 'function') {
-    throw new TypeError('Parameter "knex" must be an instance of Knex.Client. Got ' + typeof knex);
+    throw new TypeError(`Parameter "knex" must be an instance of Knex.Client. Received ${typeof knex}`);
   }
+};
 
-  return (req, res, next): void => {
-    knex.transaction((trx): void => {
-      res.locals.trx = trx;
-      next();
-    });
+const checkLocals = (res: Response): void => {
+  if (!res.locals.trx || typeof res.locals.trx.commit !== 'function' || typeof res.locals.trx.rollback !== 'function') {
+    throw new Error(
+      'res.locals.trx is not of type Knex.Transaction. Ensure that the `knexTrxBegin()` middleware ran successfully',
+    );
+  }
+};
+
+const checkMiddlewares = (middlewares: RequestHandlerParams): void => {
+  const isValidMiddleware = middlewares && (!Array.isArray(middlewares) && typeof middlewares === 'function');
+  const isValidMiddlewaresArray =
+    Array.isArray(middlewares) &&
+    middlewares &&
+    middlewares.length > 0 &&
+    middlewares.reduce((acc, curr): boolean => acc && typeof curr === 'function', true);
+
+  if (!isValidMiddleware && !isValidMiddlewaresArray) {
+    throw new TypeError(
+      `Parameter "middlewares" must be of type function or function[]. Received ${typeof middlewares}`,
+    );
+  }
+};
+
+export const knexTrxBegin = (knex: Knex): RequestHandler => {
+  checkKnex(knex);
+  return async (req, res, next): Promise<void> => {
+    res.locals.trx = await knex.transaction();
+    next();
   };
 };
 
 export const knexTrxCommit: RequestHandler = async (req, res, next): Promise<void> => {
-  if (res.locals.trx && typeof res.locals.trx.commit === 'function') {
-    try {
-      await res.locals.trx.commit();
-    } catch (err) {
-      next(err);
-      return;
-    }
+  try {
+    checkLocals(res);
+    await res.locals.trx.commit();
+    next();
+  } catch (err) {
+    next(err);
   }
-  next();
 };
 
 export const knexTrxErrorHandler: ErrorRequestHandler = async (err, req, res, next): Promise<void> => {
-  if (res.locals.trx && typeof res.locals.trx.rollback === 'function') {
-    try {
-      await res.locals.trx.rollback();
-    } catch (err) {
-      next(err);
-      return;
-    }
+  try {
+    checkLocals(res);
+    await res.locals.trx.rollback();
+  } catch (rollbackErr) {
+    // Keep original error, as that's probably the more important error.
+    err.message = `An error occurred while trying to perform a knex trx rollback due to a previous error: ${err.message}`;
+    next(err);
   }
   next(err);
 };
 
 export const withKnexTrx = (knex: Knex, middlewares: RequestHandlerParams): RequestHandlerParams => {
-  let middlewaresArray = middlewares;
-  if (!Array.isArray(middlewaresArray)) {
-    middlewaresArray = [middlewaresArray];
-  }
+  checkKnex(knex);
+  checkMiddlewares(middlewares);
 
+  const middlewaresArray = Array.isArray(middlewares) ? middlewares : [middlewares];
   return [knexTrxBegin(knex), ...middlewaresArray, knexTrxCommit, knexTrxErrorHandler];
 };
